ce208edff3266ab7d6820ea0afa66917a1b1fd37
Sidharth Anand <sidharthanand67@gmail.com>
2022-01-13 22:07:36 +0530

    Added encryption logic & decryption logic

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // Variable declarations 
 let shift_key = document.getElementById("shift-key"); 
-let message = document.getElementById("message").value; 
-
+let message_inp = document.getElementById("message"); 
+let submit_btn = document.getElementById("submit-btn"); 
+let result_box = document.getElementById("result-box"); 
+let checked_mode;
+let key; 
+let message; 
+let result; 
 
 // Function definitions 
 function fillShiftKey() {
@@ -13,8 +18,73 @@ function fillShiftKey() {
     shift_key.innerHTML = html; 
 }
 
+function encryption(message, key) {
+    let dummyResult = [], temp; 
+    for(let i = 0; i < message.length; i++){
+        temp = message.charCodeAt(i); 
+        if (temp == 32) {
+            dummyResult.push(message[i]); 
+        } 
+        else {
+            if (temp + key > 126) {
+                temp = ((temp+key) % 126 + 33) - 1; 
+            }
+            else {
+                temp = temp + key; 
+            }
+            dummyResult.push(String.fromCharCode(temp)); 
+        }
+    }
+    return dummyResult.join(''); 
+}
+
+function decryption(message, key) {
+    let dummyResult = [], temp; 
+    for(let i = 0; i < message.length; i++){
+        temp = message.charCodeAt(i); 
+        if (temp == 32){
+            dummyResult.push(message[i]); 
+        }
+        else{
+            if (temp - key < 33) {
+                temp = ((temp-key) % 126 + 33) - 1; 
+            }
+            else {
+                temp = temp - key; 
+            }
+            dummyResult.push(String.fromCharCode(temp)); 
+        }
+    }
+    return dummyResult.join(''); 
+}
+
+function printResult(){
+    if (message_inp.value !== ""){
+        message = message_inp.value;
+        checked_mode = document.querySelector('input[name="mode"]:checked').value
+        key = parseInt(document.querySelector("#shift-key").value); 
+    
+        if (checked_mode == 0){
+            result = encryption(message, key); 
+        }
+        else {
+            result = decryption(message, key); 
+        }
+    
+        console.log(result); 
+
+
+
+        // console.log(message); 
+        console.log(key); 
+        // console.log(checked_mode); 
+    } 
+}
+
+
 
 
 
 // Function Calls
 fillShiftKey(); 
+submit_btn.addEventListener("click", printResult); 
